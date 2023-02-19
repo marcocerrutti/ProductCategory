3683a459afee816e2e69ae8b486f39a1738be363
Nwaigwe Uchenna <hermessuffice@gmail.com>
2023-02-19 09:03:44 +0100

    Add project files.

---
+++ ProductCategory/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using ProductCategory.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); 
+   
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else 
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
